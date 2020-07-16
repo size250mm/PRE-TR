5d7a3bc0042cd821d9e6fdcd63d44fbf164bb510
parkjaehyeon <size210mm@naver.com>
2020-07-16 14:40:18 +0900

    fix2

--- TRAIRLINE/SCRIPT/script.js
+++ TRAIRLINE/SCRIPT/script.js
@@ -105,7 +105,7 @@ $(window).on('scroll', function(){
                         },1000,function(){
 //                           window.open('indexTRAIRLINE.html','_self')
                             setTimeout(function() {
-	window.open('indexcgv.html','_self');
+	window.open('https://size250mm.github.io/PRE-CGV/.','_self');
       }, 1000);
   ;
                         })
